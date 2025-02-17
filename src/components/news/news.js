720ed171a014d592021a71c375af7d01034250f1
JatinSachdeva2004 <jatinsachdeva3004@gmail.com>
2025-02-18 00:58:47 +0530

    JSON file updated 1

--- src/components/news/news.js
+++ src/components/news/news.js
@@ -15,7 +15,7 @@ const News = () => {
     }
 
     // Build API URL
-    let apiUrl = `https://newsapi.org/v2/everything?q=tesla&from=2025-01-17&sortBy=publishedAt&apiKey=${API_KEY}`;
+    let apiUrl = `https://newsapi.org/v2/everything?q=tesla&from=2025-01-17&sortBy=publishedAt&apiKey=68048bc0d0a74a7e84b30cab2bea21c3}`;
     if (category !== "all") {
       apiUrl += `&category=${category}`;
     }
